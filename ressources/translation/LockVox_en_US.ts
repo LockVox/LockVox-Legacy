e13a4fd94af1c48784743f6efa36d2cd18462ef0
ValentinGrim <monnot@et.esiea.fr>
2020-09-30 18:55:06 +0200

    Update LockVox_en_US.ts

--- ressources/translation/LockVox_en_US.ts
+++ ressources/translation/LockVox_en_US.ts
@@ -1,3 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="LockVox_en_US"></TS>
+<!DOCTYPE TS><TS version="2.1" language="LockVox_en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+</context>
+</TS>
