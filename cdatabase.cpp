179c558ee11314fb30ac4683b0defd802222e9c4
Tagaaa <48995110+Tagaaa@users.noreply.github.com>
2020-09-30 11:53:34 +0200

    pour val

--- cdatabase.cpp
+++ cdatabase.cpp
@@ -2,7 +2,7 @@
 
 CDatabase::CDatabase()
 {
-        m_db = QSqlDatabase::addDatabase("QMYSQL");
+        m_db = QSqlDatabase::addDatabase("QSQLITE");
         m_db.setHostName("192.168.1.13");
         m_db.setDatabaseName("lockvox");
         m_db.setUserName("lockvox");
