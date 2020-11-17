c1d52c0ca48075c141f8e48a69f9ebc65b0edc88
Tagaaa <48995110+Tagaaa@users.noreply.github.com>
2020-11-17 14:42:41 +0100

    server.pro

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -6,6 +6,47 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    //Initialize widgets
+
+
+
+
+
+
+
+    //Connect widget to action
+
+
+
+
+    //Initialize server
+    m_server = new CServer();
+
+
+    m_socket = new QTcpSocket();
+
+    m_socket->abort();
+    m_socket->connectToHost("127.0.0.1", 50885);
+
+
+
+
+
+
+
+    m_audio_in = new AudioInput();
+    m_audio_out = new AudioOutput();
+
+    m_audio_in->initializeAudioInput();
+    m_audio_out->initializeAudioOutput();
+
+
+
+
+
+
 }
 
 MainWindow::~MainWindow()
--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+#include "src/includes/cserver.h"
+#include "src/includes/audioinput.h"
+#include "src/includes/audiooutput.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -16,6 +20,23 @@ public:
     ~MainWindow();
 
 private:
+
+
+
     Ui::MainWindow *ui;
+
+
+    //Network
+
+    QTcpSocket * m_socket;
+
+
+
+
+
+    //Audio
+    AudioInput * m_audio_in;
+    AudioOutput * m_audio_out;
+
 };
 #endif // MAINWINDOW_H
