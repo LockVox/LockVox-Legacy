2697be772fb65cb4336bacb3cfa9f26d072f3e41
ValentinGrim <34299375+ValentinGrim@users.noreply.github.com>
2020-11-18 16:12:47 +0100

    Update main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-    /*QApplication a(argc, argv);
+    QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();*/
-    startServer(argc, argv);
+    return a.exec();
+    //startServer(argc, argv);
 }
