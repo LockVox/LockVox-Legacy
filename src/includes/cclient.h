0d98139c7141f463e1bd2f501fd56b0e6e8b2cb2
Valentin <monnot@et.esiea.fr>
2021-01-19 11:18:16 +0100

    Quand c'est cassé je peut réparer

--- src/includes/cclient.h
+++ src/includes/cclient.h
@@ -1,52 +1,68 @@
-#ifndef CROLE_H
-#define CROLE_H
+#ifndef CCLIENT_H
+#define CCLIENT_H
 
-#include <QString>
-#include <QList>
-#include <QDebug>
+#include <QObject>
+#include <QtNetwork>
 
-class crole
+#include "cchannel.h"
+
+class CClient
 {
-public:
-    crole();
-    crole(crole* p_father);
+    public:
+        CClient();                                          //Constructeur par défault
+        CClient(const CClient & copy);                      //Constrcteur par copie
+        CClient(int id, QString name, QTcpSocket * soc, int idChannel);    //Constructeur perso
+        CClient(QTcpSocket * soc);
 
-    //Parent+enfants
-    void AddFather(crole* p_father);
-    void AddChild(crole* p_child);
-    void DelChild(QString p_name);
-    void DelChild(int p_id);
-
-    //Getter+Setter
-    void SetWrite(bool v);
-    void SetSpeak(bool v);
-    void SetKick(bool v);
-    void SetDelete(bool v);
-    void SetSee(bool v);
-    void SetMute(bool v);
-
-    bool GetWrite();
-    bool GetSpeak();
-    bool GetKick();
-    bool GetDelete();
-    bool GetSee();
-    bool GetMute();
+        ~CClient() {};                                      //Destructeur
 
 
-private:
-    int m_id;                   //ID du role
-    QString m_name;             //Nom du role
-    crole* m_father;            //Le parent du role
-    QList<crole*> m_childrens;  //Les enfants du role
-    /*Chaque bool correspond à un droit associé au groupe*/
-    bool m_write;
-    bool m_speak;
-    bool m_kick;
-    bool m_delete;
-    bool m_see;
-    bool m_mute;
 
-    void SetId();
+        //Getters
+        QString get_pseudo();
+        QTcpSocket * get_socket();
+        int get_idChannel();
+        int get_id();
+
+
+
+        //Setters
+        void set_pseudo(QString pseudo);
+        void set_socket(QTcpSocket * soc);
+        void set_idChannel(int id);
+        void set_id(int id);
+        void set_all(CClient *c);
+
+
+
+
+        //Optionnal
+        QByteArray serialize();
+        void deserialize(QByteArray & in);
+
+        //Serialize | Deserialize
+        QJsonObject serializeToObj();
+        void deserialize(QJsonObject json_obj);
+
+
+
+
+
+
+    private slots:
+
+
+    private:
+        QString m_pseudo;
+        QTcpSocket * m_soc;
+
+        int m_id;
+        int m_idChannel;
+
+
 };
 
-#endif // CROLE_H
+
+
+
+#endif // CCLIENT_H
--- src/includes/cpacket.h
+++ src/includes/cpacket.h
@@ -1,53 +1,37 @@
 #ifndef CPACKET_H
 #define CPACKET_H
 
-#include "cclient.h"
-#include "cchannel.h"
-#include "cserver.h"
 #include <QString>
-#include <QJsonArray>
 
+#include "src/includes/cchannel.h"
+#include "src/includes/cclient.h"
 
 class CPacket
 {
 public:
     CPacket();
+    char SetType(QString m_type);
+    char ServerAction(QString m_action);
+    char ChannelAction(QString m_action);
+    char UserAction(QString m_action);
 
-    //Getters
-    char GetType();
-    char GetAction();
-    CClient GetSender();
-
-    //Setters
-    void SetType(char p_type);
-    void SetAction(char p_action);
-
-    //Text<-->code
-    QString TypeDecode(char p_type);
-    QString ServerDecode(char p_action);
-    QString ChannelDecode(char p_action);
-    QString UserDecode(char p_action);
-
-    char TypeEncode(QString p_type);
-    char ServerEncode(QString p_action);
-    char ChannelEncode(QString p_action);
-    char UserEncode(QString p_action);
-
-    //Serialiaze
-    QByteArray Serialize();
-
-
-    //Deserialize
-    void Deserialize(QByteArray in);
-    CClient DeserializeClient(QJsonArray  in);
-    CChannel DeserializeChannel(QJsonArray  in);
-    CServer DeserializeServer(QJsonArray  in);
+    enum Type {Server,
+              Channel,
+              Client};
+    enum ActServer{Serv_Connect,
+                   Serv_Disconnect,
+                   Serv_Nickname,
+                   Serv_Description,
+                   Serv_Ban,
+                   Serv_IPBan,
+                   Serv_Kick};
+    enum ActChannel{Chan_Connect,
+                    Chan_Disconnect,
+                    Chan_Send};
 
 private:
-    char m_type;
-    char m_action;
-    CClient m_client;
-    QJsonDocument m_data;
+    char header[2];
+
 };
 
 #endif // CPACKET_H
