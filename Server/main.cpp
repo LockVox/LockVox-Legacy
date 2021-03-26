b81c6cbb87a1506f4983d6cebcfb2d27f7dba292
Hugo LAVEAU <laveau@et.esiea.fr>
2021-03-26 11:23:49 +0100

    server audio qui run

--- Server/main.cpp
+++ Server/main.cpp
@@ -6,14 +6,25 @@
 #include "mainwindow.h"
 
 #include "Server/config.h"
+#include "winsock.h"
+#pragma comment(lib,"ws2_32.lib")
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+   /* QApplication a(argc, argv);
 
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return a.exec();*/
+    WSADATA Data;
+    WSAStartup(MAKEWORD(2,2), &Data);
+    CChannel tmpchan("", 0);
+    CAudio tmp;
+    tmp.AddSession(tmpchan);
+    getc(stdin);
+    WSACleanup();
+    return 0;
 }
 
